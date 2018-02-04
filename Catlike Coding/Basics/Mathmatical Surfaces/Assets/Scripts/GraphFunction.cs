786bdcadd4738543533084309dadbbf5ed6527bd
Medill-East <NEMailLHD@163.com>
2018-02-04 21:25:52 +0800

    Mathmatical Surfaces
    20180204 2125

---
+++ Catlike Coding/Basics/Mathmatical Surfaces/Assets/Scripts/GraphFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate Vector3 GraphFunction(float u,float v, float t);
+
+//public class GraphFunction : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+		
+//	}
+	
+//	// Update is called once per frame
+//	void Update () {
+		
+//	}
+//}
---
+++ Catlike Coding/Basics/Mathmatical Surfaces/Assets/Scripts/GraphFunctionName.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GraphFunctionName
+{
+    Sine,
+    Sine2D,
+    MultiSine,
+    MultiSine2D,
+    Ripple,
+    Cylinder,
+    Sphere,
+    Torus
+}
+
+//public class GraphFunctionName : MonoBehaviour {
+
+//	// Use this for initialization
+//	void Start () {
+		
+//	}
+	
+//	// Update is called once per frame
+//	void Update () {
+		
+//	}
+//}
