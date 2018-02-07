72a69b1f3e3d8d5dc3d29e0a096e52b3d05a3e24
Medill-East <NEMailLHD@163.com>
2018-02-07 23:29:51 +0800

    Frame Per Second
    20180207 2329

---
+++ Catlike Coding/Basics/Frame Per Second/Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float timeBetweenSpawn;
+    public float spawnDistance;
+    public float timeSinceLastSpawn;
+
+    public Nucleon[] nucleonPrefabs;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn > timeBetweenSpawn)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawn;
+            SpawnNucleon();
+        }
+    }
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
