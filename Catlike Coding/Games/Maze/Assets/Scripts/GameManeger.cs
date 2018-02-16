7d99648e02ce15425ca439724e0b1112af10374b
Medill-East <NEMailLHD@163.com>
2018-02-16 23:55:27 +0800

    Maze
    20180216 2355

---
+++ Catlike Coding/Games/Maze/Assets/Scripts/GameManeger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManeger : MonoBehaviour
+{
+
+    private Maze mazeInstance;
+
+    public Maze mazePrefab;
+    // Use this for initialization
+    void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame()
+    {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
---
+++ Catlike Coding/Games/Maze/Assets/Scripts/Maze.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+
+    private MazeCell[,] cells;
+
+    public int sizeX, sizeZ;
+    public float generationStepDelay;
+    public MazeCell cellPrefab;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator Generate()
+    {
+        WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
+        cells = new MazeCell[sizeX, sizeZ];
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int z = 0; z < sizeZ; z++)
+            {
+                yield return delay;
+                CreateCell(x, z);
+            }
+        }
+    }
+
+    private void CreateCell(int x, int z)
+    {
+        MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
+        cells[x, z] = newCell;
+        newCell.name = "Maze Cell " + x + ", " + z;
+        newCell.transform.parent = transform;
+        newCell.transform.localPosition = new Vector3(x - sizeX * 0.5f + 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
+    }
+}
