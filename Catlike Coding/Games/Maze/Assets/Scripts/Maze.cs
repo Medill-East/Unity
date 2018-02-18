7be5920791e7e25d3460a167f60bc926363f5da1
Medill-East <NEMailLHD@163.com>
2018-02-19 01:11:50 +0800

    Maze
    201802190111

--- Catlike Coding/Games/Maze/Assets/Scripts/Maze.cs
+++ Catlike Coding/Games/Maze/Assets/Scripts/Maze.cs
@@ -36,28 +36,58 @@ public class Maze : MonoBehaviour
         return cells[coordinate.x, coordinate.z];
     }
 
-    public IEnumerator Generate () {
+    public IEnumerator Generate ()
+    {
 		WaitForSeconds delay = new WaitForSeconds(generationStepDelay);
 		cells = new MazeCell[size.x, size.z];
-		IntVector2 coordinates = RandomCoordinates;
-		while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
-			yield return delay;
-			CreateCell(coordinates);
-			coordinates += MazeDirections.RandomValue.ToIntVector2();
-		}
+        //IntVector2 coordinates = RandomCoordinates;
+        //while (ContainsCoordinates(coordinates) && GetCell(coordinates) == null) {
+        //	yield return delay;
+        //	CreateCell(coordinates);
+        //	coordinates += MazeDirections.RandomValue.ToIntVector2();
+        List<MazeCell> activeCells = new List<MazeCell>();
+        DoFirstGenerationStep(activeCells);
+        while (activeCells.Count > 0)
+        {
+            yield return delay;
+            DoNextGenerationStep(activeCells);
+        }
 	}
+
+    private void DoNextGenerationStep(List<MazeCell> activeCells)
+    {
+        int currentIndex = activeCells.Count - 1;
+        MazeCell currentCell = activeCells[currentIndex];
+        MazeDirection direction = MazeDirections.RandomValue;
+        IntVector2 coordinates = currentCell.coordinates + direction.ToIntVector2();
+        if(ContainsCoordinates(coordinates) && GetCell(coordinates) == null)
+        {
+            activeCells.Add(CreateCell(coordinates));
+        }
+        else
+        {
+            activeCells.RemoveAt(currentIndex);
+        }
+    }
+
+    private void DoFirstGenerationStep(List<MazeCell> activeCells)
+    {
+        activeCells.Add(CreateCell(RandomCoordinates));
+    }
+
 
     public bool ContainsCoordinates(IntVector2 coordinate)
     {
         return coordinate.x >= 0 && coordinate.x < size.x && coordinate.z >= 0 && coordinate.z < size.z;
     }   
 
-    private void CreateCell(IntVector2 coordinates)
+    private MazeCell CreateCell(IntVector2 coordinates)
     {
         MazeCell newCell = Instantiate(cellPrefab) as MazeCell;
         cells[coordinates.x, coordinates.z] = newCell;
         newCell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
         newCell.transform.parent = transform;
         newCell.transform.localPosition = new Vector3(coordinates.x - size.x * 0.5f + 0.5f, 0f, coordinates.z - size.z * 0.5f + 0.5f);
+        return newCell;
     }
 }
