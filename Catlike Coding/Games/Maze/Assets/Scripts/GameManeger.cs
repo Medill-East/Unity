550144f6894b1042e6a361216a3f7b85ea5d42f7
Medill-East <NEMailLHD@163.com>
2018-03-01 00:13:30 +0800

    Maze
    Before Opening Doors
    20180301 0013

--- Catlike Coding/Games/Maze/Assets/Scripts/GameManeger.cs
+++ Catlike Coding/Games/Maze/Assets/Scripts/GameManeger.cs
@@ -29,10 +29,14 @@ public class GameManeger : MonoBehaviour
 
     private IEnumerator BeginGame()
     {
+        Camera.main.clearFlags = CameraClearFlags.Skybox;
+        Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
         mazeInstance = Instantiate(mazePrefab) as Maze;
         yield return StartCoroutine(mazeInstance.Generate());
         playerInstance = Instantiate(playerPrefab) as Player;
         playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+        Camera.main.clearFlags = CameraClearFlags.Depth;
+        Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
     }
 
     private void RestartGame()
--- Catlike Coding/Games/Maze/Assets/Scripts/Player.cs
+++ Catlike Coding/Games/Maze/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     private MazeCell currentCell;
+    private MazeDirection currentDirection;
 
     public void SetLocation(MazeCell cell)
     {
@@ -23,21 +24,35 @@ public class Player : MonoBehaviour {
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Move(MazeDirection.North);
+            Move(currentDirection);
         }
-        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        else if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Move(MazeDirection.East);
+            Move(currentDirection.GetNextClockwise());
         }
-        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        else if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
-            Move(MazeDirection.Sourth);
+            Move(currentDirection.GetOpposite());
         }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        else if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            Move(MazeDirection.West);
+            Move(currentDirection.GetNextCounterclockwise());
         }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Look(currentDirection.GetNextCounterclockwise());
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Look(currentDirection.GetNextClockwise());
+        }
+    }
+
+    private void Look(MazeDirection direction)
+    {
+        transform.localRotation = direction.ToRotation();
+        currentDirection = direction;
     }
 }
